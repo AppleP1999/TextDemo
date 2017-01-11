8c36b9484b79fe1190d88d38ea83056065c4d43f
AppleP1999 <cyberv@ApplePen.local>
2017-01-11 14:14:45 +0800

    打断

---
+++ MyCarthage/MyCarthageView.m
@@ -0,0 +1,18 @@
+//
+//  MyCarthageView.m
+//  MyCarthageDemo
+//
+//  Created by PRO on 16/3/26.
+//  Copyright © 2016年 PRO. All rights reserved.
+//
+
+#import "MyCarthageView.h"
+
+@implementation MyCarthageView
+
+- (NSString *)getCarthageSkill {
+    _carthageSkill = @"get skill -> Carthage";
+    return _carthageSkill;
+}
+
+@end
---
+++ TEXTDemoFramework/TEXTDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  TEXTDemoFramework.h
+//  TEXTDemoFramework
+//
+//  Created by hf on 2017/1/11.
+//  Copyright © 2017年 cyw_dj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TEXTDemoFramework.
+FOUNDATION_EXPORT double TEXTDemoFrameworkVersionNumber;
+
+//! Project version string for TEXTDemoFramework.
+FOUNDATION_EXPORT const unsigned char TEXTDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TEXTDemoFramework/PublicHeader.h>
+
+
